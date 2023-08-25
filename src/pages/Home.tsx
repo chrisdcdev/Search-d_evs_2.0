95c34b01e32e8bfb72e446b50242548e55f5fcb7
michelmartins9164 <michelmartins@gmail.com>
2023-08-25 09:25:55 -0300

    feature: Adição das telas responsivas, correção de bugs e lint da aplicação

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,22 @@
 import { Box } from "@chakra-ui/react";
 import Title from "../view/components/Title";
 import Search from "../view/components/Search";
-
+import { changeStatus } from "../store";
+import { useDispatch } from "react-redux";
+import { useEffect } from "react";
 export default function Home() {
+  const dispatch = useDispatch();
+  function changeStatusUserInfo() {
+    dispatch(changeStatus(false))
+  }
+  useEffect(() => {
+    changeStatusUserInfo()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
   return (
     <Box>
-      <Box>
-        <Title title="Search d_evs"/>
+      <Box display="flex" justifyContent="center">
+        <Title title="Search" sufix="d_evs" font="6xl" />
       </Box>
       <Search />
     </Box>
