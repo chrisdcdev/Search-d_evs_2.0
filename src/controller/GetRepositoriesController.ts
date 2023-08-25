2c312c7333ab6e14bcdec945e1d471f3aeb6d317
michelmartins9164 <michelmartins@gmail.com>
2023-08-25 09:49:27 -0300

    build: rodando script de build

--- src/controller/GetRepositoriesController.ts
+++ src/controller/GetRepositoriesController.ts
@@ -3,9 +3,12 @@ import { getUserRepositories } from "../services/api/repos";
 export const GetByUserRepositories = (name:string) => {
     return getUserRepositories.get(`${name}/repos`).then((Response) => {
         const response = Response.data;
-        const data = response.sort((initialCountStar, compareCountStar) => {
-            return compareCountStar.stargazers_count - initialCountStar.stargazers_count;
-        })
+        const data = response.sort((
+            initialCountStar : { stargazers_count: number }, 
+            compareCountStar : { stargazers_count: number }) => {
+            return initialCountStar.stargazers_count - compareCountStar.stargazers_count;
+          });
+          
         return data;
     })
 }
