71849d5970161e6fe36b73672ee85ed0260001a7
ccastro01 <alvescastro00cca13@gmail.com>
2024-02-06 21:59:03 -0300

    criando scroll infinito com o IntersectionObserver

--- src/controller/GetRepositoriesController.ts
+++ src/controller/GetRepositoriesController.ts
@@ -1,14 +1,14 @@
 import { getUserRepositories } from "../services/api/repos";
 
-export const GetByUserRepositories = (name:string) => {
-    return getUserRepositories.get(`${name}/repos`).then((Response) => {
+export const GetByUserRepositories = (name:string, currentPage: number) => {
+    return getUserRepositories.get(`${name}/repos?per_page=7&page${currentPage}`).then((Response) => {
         const response = Response.data;
-        const data = response.sort((
-            initialCountStar : { stargazers_count: number }, 
-            compareCountStar : { stargazers_count: number }) => {
-            return compareCountStar.stargazers_count - initialCountStar.stargazers_count;
-          });
+        // const data = response.sort((
+        //     initialCountStar : { stargazers_count: number }, 
+        //     compareCountStar : { stargazers_count: number }) => {
+        //     return compareCountStar.stargazers_count - initialCountStar.stargazers_count;
+        //   });
           
-        return data;
+        return response;
     })
 }
--- src/view/components/Repositories.tsx
+++ src/view/components/Repositories.tsx
@@ -2,7 +2,6 @@ import { Box, Divider, Image, Link, Text } from "@chakra-ui/react"
 import Star from "../../../public/assets/Star.svg"
 import { useSelector } from "react-redux";
 import { AppState, UserState } from "../../store";
-import { GetByUserRepositories } from "../../controller/GetRepositoriesController";
 import { RepositoriesProps } from "../../model/Repositories";
 import { useEffect, useState } from "react";
 import { DateFormate } from "../../utils/FormateDate";
@@ -12,18 +11,37 @@ function Repositories() {
     const [repositories, setRepositories] = useState<RepositoriesProps[]>([]);
     const user = useSelector((state: AppState) => state.user);
     const status = useSelector((state: UserState) => state.status);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const [currentPage, setCurrentPage] = useState(0);
 
-    const GetDataByrepo = async () => {
-        const data = await GetByUserRepositories(user);
-
-        setRepositories(data);
-
-
-    };
     useEffect(() => {
-        GetDataByrepo()
-    // eslint-disable-next-line react-hooks/exhaustive-deps
+        if (currentPage > 0) {
+            fetch(`https://api.github.com/users/${user}/repos?per_page=6&page=${currentPage}`)
+                .then((response) => response.json())
+                .then((newRepositories) => setRepositories((prevRepositories) => [...prevRepositories, ...newRepositories]))
+                .catch((error) => {
+                    console.error("Error fetching repositories:", error);
+                });
+        }
+    }, [currentPage]);
+
+    useEffect(() => {
+        const intersextionObserver = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+            if (entries.some((entry) => entry.isIntersecting)) {
+                setCurrentPage((currentPageInsideState) => currentPageInsideState + 1);
+            }
+        })
+
+        const sentinelElement = document.querySelector("#sentinel");
+
+        if (sentinelElement) {
+            intersextionObserver.observe(sentinelElement);
+        }
+    
+        return () => {
+            if (sentinelElement) {
+                intersextionObserver.unobserve(sentinelElement);
+            }
+        };
     }, [])
 
     if (!status && !user) {
@@ -33,12 +51,12 @@ function Repositories() {
     }
 
     return (
-        <Box width={{xl:"900px",lg:"500px", md:"420px",sm:"100%"}} overflow="auto" backgroundColor="#fff" height="50rem" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center">
+        <Box width={{ xl: "900px", lg: "500px", md: "420px", sm: "100%" }} overflow="auto" backgroundColor="#fff" height="50rem" display="flex" flexDirection="column" justifyContent="flex-start" alignItems="center">
             {repositories.length > 0 ?
                 repositories && repositories?.map((content: RepositoriesProps) => {
                     return (
                         <Box key={content.id}>
-                            <Box overflow="hidden" width={{xl:"850px", md:"400px", sm:"400px", base: "300px"}}>
+                            <Box overflow="hidden" width={{ xl: "850px", md: "400px", sm: "400px", base: "300px" }}>
                                 <Link href={content.clone_url} isExternal>
                                     <Text fontFamily="body" marginBottom="16px" marginTop="24px" fontSize="20px" fontWeight="700" color="#171923">{content.name}</Text>
                                 </Link>
@@ -46,18 +64,17 @@ function Repositories() {
                                 <Box display="flex" columnGap="8px" marginBottom="16px">
                                     <span><Image src={Star} /></span> <p> {content.stargazers_count}  •  {DateFormate(content.updated_at)}</p>
                                 </Box>
-                                <Divider backgroundColor="#E2E8F0" width="850px" />
                             </Box>
                         </Box>
-
                     )
                 }) :
                 <Box display="flex" alignItems="center" justifyContent="center" width="100%" height="100%">
                     <Title title="Sem" sufix="repositórios" font="3xl" />
                 </Box>
             }
+            <Divider color="red" width="100%" id="sentinel" marginBottom="1rem" />
         </Box>
-    )
+    );
 }
 
 export default Repositories
